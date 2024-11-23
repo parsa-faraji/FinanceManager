108e75a168dd164d9a774ca4c7b27eb55fd60ea0
parsa-faraji <parsafaraji8213@gmail.com>
2024-11-22 22:02:19 -0800

    create Transaction struct

---
+++ FinanceManager/Transaction.cpp
@@ -0,0 +1,8 @@
+//
+//  Transaction.cpp
+//  FinanceManager
+//
+//  Created by Parsa Faraji on 11/22/24.
+//
+
+#include "Transaction.hpp"
---
+++ FinanceManager/Transaction.hpp
@@ -0,0 +1,50 @@
+//
+//  Transaction.hpp
+//  FinanceManagementApp
+//
+//  Created by Parsa Faraji on 11/22/24.
+//
+
+#ifndef Transaction_hpp
+#define Transaction_hpp
+
+#include <string>
+#include "Date.hpp"
+#include "Account.hpp"
+#include "Time.hpp"
+
+using std::string;
+
+// enum for transaction methods
+enum class TransactionMethod {
+    InStore,
+    Online
+};
+
+// transaction structure
+struct Transaction {
+    string transactionType;  // e.g., "Purchase" or "Deposit"
+    string category;
+    Date date;
+    Time time;
+    Account account;
+    double amount;
+    TransactionMethod method;
+
+    // utility functions
+    Date getDate() const { return date; }
+    double getAmount() const { return amount; }
+    string getCategory() const { return category; }
+    TransactionMethod getMethod() const { return method; }
+    bool isValidTransaction() const;
+
+    // csv management
+    string toCSV() const;
+    static Transaction fromCSV(const string& csvLine);
+
+    // overloaded operators
+    bool operator==(const Transaction& other) const;
+    friend std::ostream& operator<<(std::ostream& os, const Transaction& transaction);
+};
+
+#endif /* Transaction_hpp */

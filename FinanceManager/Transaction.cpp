3edd824a62519b0dfd202ef68c9997abc78f6a91
parsa-faraji <parsafaraji8213@gmail.com>
2024-11-22 22:03:54 -0800

    implement toCSV function for Transaction

--- FinanceManager/Transaction.cpp
+++ FinanceManager/Transaction.cpp
@@ -17,3 +17,20 @@ using namespace std;
 bool Transaction::isValidTransaction() const {
     return !transactionType.empty() && date.isValidDate() && time.isValidTime() && !category.empty();
 }
+
+// serialize transaction to csv format
+string Transaction::toCSV() const {
+    ostringstream oss;
+    oss << transactionType << ","
+        << setfill('0') << setw(2) << date.getMonth() << "/"
+        << setfill('0') << setw(2) << date.getDay() << "/"
+        << setw(4) << date.getYear() << "," // ensure year is always 4 digits
+        << setfill('0') << setw(2) << time.getHour() << ":"
+        << setfill('0') << setw(2) << time.getMinute()
+        << setw(2) << time.getDayOrNight()
+        << account.getAccountNumber() << ","
+        << fixed << setprecision(2) << amount << ","
+        << category << ","
+        << (method == TransactionMethod::InStore ? "In-store" : "Online");
+    return oss.str();
+}

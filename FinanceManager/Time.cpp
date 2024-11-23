46a4f99c826b9584eac4813d0f47d549f86dea8b
parsa-faraji <parsafaraji8213@gmail.com>
2024-11-22 19:27:12 -0800

    implement constructors for Time structure

--- FinanceManager/Time.cpp
+++ FinanceManager/Time.cpp
@@ -1,8 +1,12 @@
-//
-//  Time.cpp
-//  FinanceManager
-//
-//  Created by Parsa Faraji on 11/22/24.
-//
-
 #include "Time.hpp"
+#include <stdexcept>
+
+// default constructor
+Time::Time() : hour(0), minute(0) {}
+
+// parameterized constructor
+Time::Time(short int hour, short int minute) : hour(hour), minute(minute) {
+    if (!isValidTime()) {
+        throw std::invalid_argument("Invalid time provided.");
+    }
+}

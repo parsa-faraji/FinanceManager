e4c6231d4c56568f563ee73888112e2c91215a63
parsa-faraji <parsafaraji8213@gmail.com>
2024-11-22 21:59:13 -0800

    create Account constructors

--- FinanceManager/Account.cpp
+++ FinanceManager/Account.cpp
@@ -1,8 +1,9 @@
-//
-//  Account.cpp
-//  FinanceManager
-//
-//  Created by Parsa Faraji on 11/22/24.
-//
-
 #include "Account.hpp"
+#include <stdexcept>
+
+// default constructor: initializes account number to 0 and balance to 0.0
+Account::Account() : accountNumber(0), balance(0.0) {}
+
+// parameterized constructor: initializes account number and balance with given values
+Account::Account(long long accountNumber, double balance)
+    : accountNumber(accountNumber), balance(balance) {}

c6a908341c06abaaf9741e5e9fe3a7339a2e90cc
parsa-faraji <parsafaraji8213@gmail.com>
2024-11-22 19:27:41 -0800

    implement setters for Time

--- FinanceManager/Time.cpp
+++ FinanceManager/Time.cpp
@@ -10,3 +10,24 @@ Time::Time(short int hour, short int minute) : hour(hour), minute(minute) {
         throw std::invalid_argument("Invalid time provided.");
     }
 }
+
+void Time::setHour(short int newHour) {
+    if (newHour < 0 || newHour > 12) {
+        throw std::invalid_argument("Hour must be between 0 and 12.");
+    }
+    hour = newHour;
+}
+
+void Time::setMinute(short int newMinute) {
+    if (newMinute < 0 || newMinute > 59) {
+        throw std::invalid_argument("Minute must be between 0 and 59.");
+    }
+    minute = newMinute;
+}
+
+void Time::setDayOrNight(string newDayOrNight) {
+    if (dayOrNight != "pm" && dayOrNight != "am") {
+        throw std::invalid_argument("Please pass am or pm in lowercase for day or night.");
+    }
+    dayOrNight = newDayOrNight;
+}

2285dbf0ecbc88996532bf60db87f207ecd8c8bd
parsa-faraji <parsafaraji8213@gmail.com>
2024-11-22 22:08:00 -0800

    create FinanceManagement namespace and categories vector

--- FinanceManager/util.cpp
+++ FinanceManager/util.cpp
@@ -1,8 +1,29 @@
 //
 //  util.cpp
-//  FinanceManager
+//  FinanceManagementApp
 //
 //  Created by Parsa Faraji on 11/22/24.
 //
 
 #include "util.hpp"
+#include "Transaction.hpp"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include <limits>
+#include <memory> // for smart pointers
+
+using namespace std;
+
+// define namespace
+namespace FinanceManagement {
+
+// define the categories list
+const vector<string> categories = {
+    "Food", "Housing", "Transport", "Healthcare",
+    "Entertainment", "Education", "Savings", "Income",
+    "Debt", "Other"
+};

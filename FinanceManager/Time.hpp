8e69ea392a9a690364287ce4e75db26498fbc342
parsa-faraji <parsafaraji8213@gmail.com>
2024-11-22 22:24:10 -0800

    modify the Time function

--- FinanceManager/Time.hpp
+++ FinanceManager/Time.hpp
@@ -8,7 +8,6 @@ using std::string;
 struct Time {
     short int hour;    // 0 to 23
     short int minute;  // 0 to 59
-    std::string dayOrNight; // to show pm or am
 
     // constructors
     Time();
@@ -17,12 +16,10 @@ struct Time {
     // setters
     void setHour(short int newHour);
     void setMinute(short int newMinute);
-    void setDayOrNight(string dayOrNight);
 
     // getters
     short int getHour() const;
     short int getMinute() const;
-    string getDayOrNight() const;
 
     // utility function
     bool isValidTime() const;

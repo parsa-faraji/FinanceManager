bd05145b0df3ae08d3a4ffe53ec5bb233e0fd9f6
parsa-faraji <parsafaraji8213@gmail.com>
2024-11-22 22:03:20 -0800

    implement isValidTransaction

--- FinanceManager/Transaction.cpp
+++ FinanceManager/Transaction.cpp
@@ -1,8 +1,19 @@
 //
 //  Transaction.cpp
-//  FinanceManager
+//  FinanceManagementApp
 //
-//  Created by Parsa Faraji on 11/22/24.
+//  Created by Parsa Faraji on 11/15/24.
 //
 
 #include "Transaction.hpp"
+#include <sstream>
+#include <iomanip>
+#include <stdexcept>
+#include <iostream>
+
+using namespace std;
+
+// check if the transaction is valid
+bool Transaction::isValidTransaction() const {
+    return !transactionType.empty() && date.isValidDate() && time.isValidTime() && !category.empty();
+}

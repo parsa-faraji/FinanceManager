4935459649d8515cf2bb68d86e731755952a9e5a
parsa-faraji <parsafaraji8213@gmail.com>
2024-11-22 19:22:57 -0800

    add constructor definitions

--- FinanceManager/Date.cpp
+++ FinanceManager/Date.cpp
@@ -1,8 +1,10 @@
-//
-//  Date.cpp
-//  FinanceManager
-//
-//  Created by Parsa Faraji on 11/22/24.
-//
-
 #include "Date.hpp"
+#include <stdexcept>
+
+// default constructor: initializes the date to 0/0/0
+Date::Date() : year(0), month(0), day(0) {}
+
+// parameterized constructor: initializes the date with provided year, month, and day values
+Date::Date(short int year, short int month, short int day)
+    : year(year), month(month), day(day) {}
+
